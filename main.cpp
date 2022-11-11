574dd6ba04d88aeacc2e23d0650561af0a61343d
Sergio Gasquez <sergio.gasquez@gmail.com>
2022-11-11 17:56:09 +0100

    style: 🔥 Remove C and unused files

--- main.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright © SixtyFPS GmbH <info@slint-ui.com>
-// SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-Slint-commercial
-
-#include "gallery.h"
-
-int main()
-{
-    App::create()->run();
-}
